3bb02ad2f225feea46db068c5685a895ca013068
Mochammad Hanif R <etrnal70@gmail.com>
2021-11-23 20:14:55 +0700

    (greetd) Change location

---
+++ .scripts/tuigreet/sway-run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Session
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
+export WLR_DRM_NO_MODIFIERS=1
+# export WLR_RENDERER=vulkan
+
+# SSH Agent
+SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+source /usr/local/bin/wayland_enablement.sh
+
+systemd-cat --identifier=sway sway $@
