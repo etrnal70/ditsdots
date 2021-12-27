069a216cae7994e682df3231b117b4159ac06844
Mochammad Hanif R <etrnal70@gmail.com>
2021-12-27 22:29:27 +0700

    (tuigreet) Enable SDL wayland by default, lint fix

--- .scripts/tuigreet/sway-run.sh
+++ .scripts/tuigreet/sway-run.sh
@@ -8,8 +8,8 @@ export WLR_DRM_NO_MODIFIERS=1
 # export WLR_RENDERER=vulkan
 
 # SSH Agent
-SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 source /usr/local/bin/wayland_enablement.sh
 
-systemd-cat --identifier=sway sway $@
+systemd-cat --identifier=sway sway "$@"
--- .scripts/tuigreet/wayland_enablement.sh
+++ .scripts/tuigreet/wayland_enablement.sh
@@ -7,8 +7,7 @@ export CLUTTER_BACKEND=wayland
 export QT_QPA_PLATFORM=wayland-egl
 export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
 export _JAVA_AWT_WM_NONPARENTING=1
-# Steam is misbehaving, so disable it for now
-# export SDL_VIDEODRIVER=wayland
+export SDL_VIDEODRIVER=wayland
 export ELM_ENGINE=wayland_egl
 export ECORE_EVAS_ENGINE=wayland-egl
 export NO_AT_BRIDGE=1
