0eef92d5ba6b75c8901f30afb62abb95f0c6bbb8
Mochammad Hanif R <etrnal70@gmail.com>
2022-11-05 16:06:10 +0700

    greetd wrapper update
    Vulkan renderer looks ready for hyprland. Some bugs on sway

--- .scripts/tuigreet/hypr-run.sh
+++ .scripts/tuigreet/hypr-run.sh
@@ -2,12 +2,12 @@
 
 # Session
 export XDG_SESSION_TYPE=wayland
-export XDG_SESSION_DESKTOP=sway
-export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP=Hyprland
+export XDG_CURRENT_DESKTOP=Hyprland
 export WLR_DRM_NO_MODIFIERS=1
-# export WLR_RENDERER=vulkan
+export WLR_RENDERER=vulkan
 
 source /usr/local/bin/wayland_enablement.sh
 source /usr/local/bin/defaults.sh
 
-systemd-cat --identifier=sway sway "$@"
+systemd-cat --identifier=sway Hyprland "$@"
