698a903451bca15e00310f3e9ed4771fd7864b26
Mochammad Hanif R <etrnal70@gmail.com>
2022-12-09 13:25:16 +0700

    (tuigreet) sync
    vulkan renderer is ready for sway

---
+++ .scripts/tuigreet/defaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# GTK Theme
+export GTK_THEME=Qogir-Dark
+
+# SSH Agent
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
--- .scripts/tuigreet/sway-run.sh
+++ .scripts/tuigreet/sway-run.sh
@@ -5,7 +5,7 @@ export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
 export XDG_CURRENT_DESKTOP=sway
 export WLR_DRM_NO_MODIFIERS=1
-# export WLR_RENDERER=vulkan
+export WLR_RENDERER=vulkan
 
 source /usr/local/bin/wayland_enablement.sh
 source /usr/local/bin/defaults.sh
