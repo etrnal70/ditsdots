b19d0eb387bba23ee7653d0f435189a016d9ed7e
Mochammad Hanif R <etrnal70@gmail.com>
2023-11-07 15:25:05 +0700

    (nvim) handle rust analyzer dap deps

--- .config/nvim/lua/config/lsp/server_configs/rust_analyzer.lua
+++ .config/nvim/lua/config/lsp/server_configs/rust_analyzer.lua
@@ -1,15 +1,7 @@
 local M = {}
 
 M.setup = function(capabilities)
-  local dir_tbl = vim.fn.glob(vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-*", _, true)
-  local ext_path = dir_tbl[#dir_tbl]
-  local codelldb_path = ext_path .. "adapter/codelldb"
-  local liblldb_path = ext_path .. "lldb/lib/liblldb.so"
-
-  require("rust-tools").setup {
-    dap = {
-      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-    },
+  local config = {
     tools = {
       autoSetHints = false,
       executor = require("rust-tools.executors").toggleterm,
@@ -26,12 +18,27 @@ M.setup = function(capabilities)
       end,
     },
     server = {
-      -- cmd = { "ra-multiplex" },
       cargo = { allFeatures = true },
       standalone = false,
       capabilities = capabilities,
     },
   }
+
+  local dir_tbl = vim.fn.glob(vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-*", _, true)
+  local ext_path = dir_tbl[#dir_tbl]
+  if ext_path ~= nil then
+    local codelldb_path = ext_path .. "adapter/codelldb"
+    local liblldb_path = ext_path .. "lldb/lib/liblldb.so"
+    vim.tbl_extend(
+      "keep",
+      config,
+      { dap = {
+        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+      } }
+    )
+  end
+
+  require("rust-tools").setup { config }
 end
 
 return M
