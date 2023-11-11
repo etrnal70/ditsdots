314fccf9ab36575030fdd691814b27834ff678ed
Mochammad Hanif R <etrnal70@gmail.com>
2023-11-11 12:27:41 +0700

    (waybar) mirroring and privacy modules

--- .config/waybar/modules/gitlab.sh
+++ .config/waybar/modules/gitlab.sh
@@ -5,7 +5,7 @@ FILE=~/.config/github/gitlab_notifications.token
 if [ -f "$FILE" ]; then
   token=$(cat $FILE)
   count=$(curl --header "PRIVATE-TOKEN: $token" https://gitlab.com/api/v4/issues?state=opened | jq '. | length')
-  
+
   if [[ "$count" != "0" ]]; then
     jo text="$count" tooltip="You have $count GitLab notifications"
   fi
